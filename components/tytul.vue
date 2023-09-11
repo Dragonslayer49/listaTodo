13970654408323ffd37f3fde3a5f340fd9a989f3
Natan S <65855892+Dragonslayer49@users.noreply.github.com>
2023-09-11 09:29:37 +0200

    z piatku/fix

---
+++ components/tytul.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const props = defineProps({
+  title: String,
+});
+</script>
+
+<template>
+  <h3 class="align-self-center dzien">{{ title }}</h3>
+</template>
+
+<style scoped></style>
